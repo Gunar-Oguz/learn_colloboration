d0991b985d7b919e04728c5730227de1bbaaf391
turalsadigov <turalsadikov@gmail.com>
2025-11-20 13:12:18 -0800

    cube-rebased

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ def main():
     print("Hello from learn-collob!")
     print("Github")
     print("End fight")
+    print("new edit here")
+    x = 3 + 5
+    print(x)
+    print(x**2)
+    print(x**3)
 
 
 if __name__ == "__main__":
